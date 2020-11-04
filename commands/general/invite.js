7984fd9ce3dcf63f6f43554d11b4802237b05771
FlaringPhoenix <joshmeubanks@gmail.com>
2020-11-04 14:48:23 -0600

    Improved invite.js

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -1,8 +1,6 @@
-const Discord = require('discord.js');
-
 exports.run = async (client, message, args, guildConf, userConf) => {
 
-    await client.sendEmbed(message.channel, `Invite me!`, `[Click-Here](https://discord.com/api/oauth2/authorize?client_id=758091636260733069&permissions=387136&scope=bot)`)
+    await client.sendEmbed(message.channel, `Invite me!`, `[Click-Here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=387136&scope=bot)`)
 
 }
 
