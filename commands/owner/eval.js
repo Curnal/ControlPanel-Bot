002b524a1a029658019691003755dea4badf28ff
josh.eubanks <Ghost31415!>
2020-11-24 16:33:10 -0600

    Added credit for eval

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
+/*
+    Credit: WilsontheWolf#0074
+ */
+
 exports.run = async (client, message, args, guildConf, userConf) => {
 
     const embed = new Discord.MessageEmbed()
