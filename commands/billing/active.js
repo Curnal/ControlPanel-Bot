dfdb5851a86fb7a674699b13bb26d9772b87cd58
josh.eubanks <Ghost31415!>
2020-11-24 21:51:12 -0600

    Fix active.js

--- commands/billing/active.js
+++ commands/billing/active.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-exports.run = async (client, message, args) => {
+exports.run = async (client, message, args, guildConf, userConf) => {
 
     return client.sendEmbed(message.channel, "✅ Active Orders", userConf.store.products.active.length === 0 ? `You have no active orders at the moment.\n\`Do ${guildConf.prefix}store\` to purchase a plan` : ((userConf.store.products.active.map((p, index) => `**${index+1}**. ${p.title} - Price: $${p.price}`).join('\n'))),);
 
